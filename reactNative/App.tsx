ed686cf88483288b8602f4f305ddc32e9e80e1e1
kris1803 <kristiansstraume17@gmail.com>
2022-09-18 18:36:23 +0200

    separate apps init

--- reactNative/App.tsx
+++ reactNative/App.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Box } from 'native-base';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, StatusBar, useColorScheme, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles/main';
-//import styled from 'styled-components/native';
 
 export default function MainScreen() {
    const [display, setDisplay] = useState('0');
@@ -48,12 +45,11 @@ export default function MainScreen() {
          setDisplay('0');
          return;
       }
-      setDisplay(display / 100);
+      setDisplay(( Number(display) / 100).toString());
    }
 
    const handleSign = () => {
-      let displayCopy = `${display}`;
-      displayCopy = displayCopy.split('');
+      let displayCopy = (`${display}`).split('');
       if (displayCopy[0] === '-') {
          displayCopy.shift();
          setDisplay(displayCopy.join(''));
@@ -69,13 +65,13 @@ export default function MainScreen() {
       let tempSecond = parseFloat(display);
       console.log(tempSecond);
       if (operator === '+') {
-         setDisplay(parseFloat(firstNumber) + tempSecond);
+         setDisplay(( parseFloat(firstNumber) + tempSecond ).toString());
       } else if (operator === '-') {
-         setDisplay(parseFloat(firstNumber) - tempSecond);
+         setDisplay(( parseFloat(firstNumber) - tempSecond ).toString());
       } else if (operator === '*') {
-         setDisplay(parseFloat(firstNumber) * tempSecond);
+         setDisplay( (parseFloat(firstNumber) * tempSecond ).toString());
       } else if (operator === '/') {
-         setDisplay(parseFloat(firstNumber) / tempSecond);
+         setDisplay( (parseFloat(firstNumber) / tempSecond ).toString());
       }
       setOperator('');
       setFirstNumber('');
@@ -84,149 +80,149 @@ export default function MainScreen() {
 
    return (
       <SafeAreaView style={styles.container} >
-         <Box style={{ width: '100%', flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end', paddingHorizontal: '5%' }} >
+         <View style={{ width: '100%', flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end', paddingHorizontal: '5%' }} >
             <Text style={styles.mainText} >{display}</Text>
-         </Box>
-         <Box style={{ width: '100%', paddingHorizontal: '1%', paddingBottom: '1.5%' }}>
-            <Box style={styles.buttonRow}>
-               <Box style={styles.buttonContainer} >
+         </View>
+         <View style={{ width: '100%', paddingHorizontal: '1%', paddingBottom: '1.5%' }}>
+            <View style={styles.buttonRow}>
+               <View style={styles.buttonContainer} >
                   <TouchableOpacity onPress={() => { clearAll() }} style={styles.buttonLight}>
                      <Text style={styles.blackButtonText} >{display === '0' ? 'AC' : 'C'}</Text>
                   </TouchableOpacity>
-               </Box>
-               <Box style={styles.buttonContainer} >
+               </View>
+               <View style={styles.buttonContainer} >
                   <TouchableOpacity onPress={() => handleSign()} style={styles.buttonLight}>
                      <Text style={styles.blackButtonText} >±</Text>
                   </TouchableOpacity>
-               </Box>
-               <Box style={styles.buttonContainer} >
+               </View>
+               <View style={styles.buttonContainer} >
                   <TouchableOpacity onPress={() => handlePercent()} style={styles.buttonLight}>
                      <Text style={styles.blackButtonText} >%</Text>
                   </TouchableOpacity>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.operatorWrapperBg} >
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.operatorWrapperBg} >
                      <TouchableOpacity onPress={() => handleOperator('/')} style={styles.buttonOperator}>
                         <Text style={styles.whiteText} >÷</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-            </Box>
-            <Box style={styles.buttonRow}>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+            </View>
+            <View style={styles.buttonRow}>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(7)} style={styles.button}>
                         <Text style={styles.whiteText} >7</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(8)} style={styles.button}>
                         <Text style={styles.whiteText} >8</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(9)} style={styles.button}>
                         <Text style={styles.whiteText} >9</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.operatorWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.operatorWrapperBg} >
                      <TouchableOpacity onPress={() => handleOperator('*')} style={styles.buttonOperator}>
                         <Text style={styles.whiteText} >×</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-            </Box>
-            <Box style={styles.buttonRow}>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+            </View>
+            <View style={styles.buttonRow}>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(4)} style={styles.button}>
                         <Text style={styles.whiteText} >4</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(5)} style={styles.button}>
                         <Text style={styles.whiteText} >5</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(6)} style={styles.button}>
                         <Text style={styles.whiteText} >6</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.operatorWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.operatorWrapperBg} >
                      <TouchableOpacity onPress={() => handleOperator('-')} style={styles.buttonOperator}>
                         <Text style={styles.whiteText} >-</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-            </Box>
-            <Box style={styles.buttonRow}>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+            </View>
+            <View style={styles.buttonRow}>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(1)} style={styles.button}>
                         <Text style={styles.whiteText} >1</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(2)} style={styles.button}>
                         <Text style={styles.whiteText} >2</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(3)} style={styles.button}>
                         <Text style={styles.whiteText} >3</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer} >
-                  <Box style={styles.operatorWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer} >
+                  <View style={styles.operatorWrapperBg} >
                      <TouchableOpacity onPress={() => handleOperator('+')} style={styles.buttonOperator}>
                         <Text style={styles.whiteText} >+</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-            </Box>
-            <Box style={styles.buttonRow}>
-               <Box style={{ width: '50%', aspectRatio: 2, alignItems: 'center', justifyContent: 'center' }} >
-                  <Box style={styles.zeroWrapperBg} >
+                  </View>
+               </View>
+            </View>
+            <View style={styles.buttonRow}>
+               <View style={{ width: '50%', aspectRatio: 2, alignItems: 'center', justifyContent: 'center' }} >
+                  <View style={styles.zeroWrapperBg} >
                      <TouchableOpacity onPress={() => handleNumber(0)} style={styles.zeroButton}>
                         <Text style={styles.whiteText} >0</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer}>
-                  <Box style={styles.buttonWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer}>
+                  <View style={styles.buttonWrapperBg} >
                      <TouchableOpacity onPress={() => setDisplay((value) => value+'.')} style={styles.button}>
                         <Text style={styles.whiteText} >,</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-               <Box style={styles.buttonContainer}>
-                  <Box style={styles.operatorWrapperBg} >
+                  </View>
+               </View>
+               <View style={styles.buttonContainer}>
+                  <View style={styles.operatorWrapperBg} >
                      <TouchableOpacity onPress={() => handleExecute()} style={styles.buttonOperator}>
                         <Text style={styles.whiteText} >=</Text>
                      </TouchableOpacity>
-                  </Box>
-               </Box>
-            </Box>
-         </Box>
-         <StatusBar style={'light'} />
+                  </View>
+               </View>
+            </View>
+         </View>
+         <StatusBar />
       </SafeAreaView>
    )
 }
